99f36165c0d540ce4f434abfb5f3ca42b1dc4a94
Ismaili Hajir (ismhaj01) <ismhaj01@students.zhaw.ch>
2024-10-03 15:32:20 +0200

    solved aufg2b

---
+++ LE03/hajir/Aufg2b.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def g(x):
+    return x / (np.sin(1 + x) - np.sin(1))
+
+x_values = np.arange(-1e-14, 1e-14, 1e-17)
+
+g_values = g(x_values)
+
+plt.plot(x_values, g_values, label='g(x) = x * (sin(1 + x) - sin(1))')
+plt.xlabel('x')
+plt.ylabel('g(x)')
+plt.title('Plot of g(x) over the interval [-10^-14, 10^-14]')
+plt.legend()
+plt.grid(True)
+plt.show()
