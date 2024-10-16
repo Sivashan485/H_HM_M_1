316b189eebbc74e6b44cb05573d61fcde2f0540b
Ismaili Hajir (ismhaj01) <ismhaj01@students.zhaw.ch>
2024-10-16 16:27:58 +0200

    commmmiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiit

--- LE04/hajir/Aufgabe3.py
+++ LE04/hajir/Aufgabe3.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # Define the function g(x)
 def g(x):
-    return 105 * (2 * np.e) ** (-x / 100)
+    return 10**5 * (2 * np.e ** (-x / 100))
+
 
 # Create an array of x values from 0 to 100
 x = np.linspace(0, 100, 500)
@@ -13,20 +15,20 @@ y = g(x)
 
 # Plot the function normally
 plt.figure()
-plt.plot(x, y, label='g(x) = 105 * (2e)^(-x/100)')
-plt.xlabel('x')
-plt.ylabel('g(x)')
-plt.title('Normal Plot of g(x)')
+plt.plot(x, y, label="g(x) = 10**5 * (2e)^(-x/100)")
+plt.xlabel("x")
+plt.ylabel("g(x)")
+plt.title("Normal Plot of g(x)")
 plt.legend()
 plt.grid(True)
 plt.show()
 
 # Plot the function using plt.semilogy
 plt.figure()
-plt.semilogy(x, y, label='g(x) = 105 * (2e)^(-x/100)')
-plt.xlabel('x')
-plt.ylabel('g(x)')
-plt.title('Semilog Plot of g(x)')
+plt.semilogy(x, y, label="g(x) = 10**5 * (2e)^(-x/100)")
+plt.xlabel("x")
+plt.ylabel("g(x)")
+plt.title("Semilog Plot of g(x)")
 plt.legend()
 plt.grid(True)
 plt.show()
