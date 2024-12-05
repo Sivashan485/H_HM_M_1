91d16efedfc8ca55b9a3ece71067a19b975773ad
Ismaili Hajir (ismhaj01) <ismhaj01@students.zhaw.ch>
2024-12-05 15:49:37 +0100

    solved hm s11

--- SE11/HAJIR/Serie11_Aufg3_Gerüst.py
+++ SE11/HAJIR/Serie11_Aufg3_Gerüst.py
@@ -1,37 +1,41 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 29 14:43:17 2020
-
-Höhere Mathematik 1, Serie 11, Aufgabe 3 (Gerüst)
-
-@author: knaa
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-detail = 1000                       # number of pixels in x and y direction
-maxit = 120                         # maximum n for iterations (influences how detailed the structures are shown when zooming in)
-x_min = -2.0
-x_max = 1.0
-y_min = -1.5
-y_max = 1.5
+# Parameters
+detail = 1000  # Number of pixels in x and y direction
+maxit = 120  # Maximum number of iterations
+x_min, x_max = -2, 0.7  # Bounds for the x-axis
+y_min, y_max = -1.4, 1.4  # Bounds for the y-axis
 
-a = np.linspace(x_min, x_max, detail, dtype=np.float64)  # define real axis [x_min, x_max]
-b = np.linspace(y_min, y_max, detail, dtype=np.float64)  # define imaginary axis [y_min, y_max]
+# Define the real and imaginary axis
+a = np.linspace(x_min, x_max, detail, dtype=np.float64)
+b = np.linspace(y_min, y_max, detail, dtype=np.float64)
 
-B = np.zeros((detail, detail))        # for color values n 
+# Create a grid for the complex plane
+B = np.zeros((detail, detail))  # For storing color values
+x, y = np.meshgrid(a, b)
+C = x + y * 1j  # Create the complex plane
+Z = np.zeros_like(C, dtype=np.complex128)  # Initial condition
 
-[x, y] = np.meshgrid(a, b)       # to create the complex plane with the axes defined by a and b
-C = np.array(x + y * 1j, np.complex128)     # creating the plane
-Z = np.zeros(C.shape, np.complex128)  # initial conditions (first iteration), Z has same dimension as C
-for n in np.arange(1, maxit + 1):       # start iteration
-    Z = Z**2 + C
-    expl = np.abs(Z) > 2         # finding exploded values (i.e. with an absolute value > 2)
-    Z[expl] = 0                        # removing from iteration
-    C[expl] = 0                        # removing from plane
-    B[expl] = n
+# Mandelbrot iteration
+for n in range(1, maxit + 1):
+    Z = Z**2 + C  # Iteration formula
+    expl = np.abs(Z) > 2  # Check where |Z| > 2
+    Z[expl] = 0  # Remove exploded values from Z
+    C[expl] = 0  # Remove exploded values from C
+    B[expl] = n  # Save the iteration count
 
-plt.figure(1)
-B = B / np.max(np.max(B))           # dividing by max value for correct color
-plt.imshow(B, extent=[x_min, x_max, y_min, y_max], origin='lower', interpolation='bilinear')   # display image
+# Normalize the color values for display
+B = B / np.max(B)
+
+# Plot the Mandelbrot set
+plt.figure(figsize=(10, 10))
+plt.imshow(B, extent=[x_min, x_max, y_min, y_max], origin='lower', interpolation='bilinear', cmap='hot')
+plt.colorbar(label="Iteration count")
+plt.title("Mandelbrot Set")
+plt.xlabel("Re(c)")
+plt.ylabel("Im(c)")
+
+print("Displaying plot...")
 plt.show()
+print("Plot displayed.")
